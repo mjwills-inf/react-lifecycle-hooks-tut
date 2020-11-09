c409cfc1709d3a282d0d6e4b1b363d44e8686dc7
mjwills-inf <mjwills@live.co.uk>
2020-11-09 11:38:22 +0000

    .

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import Counter from './components/counter'
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mount:true
+    }
+
+    this.mountCounter = () => this.setState({mount: true})
+    this.unmountCounter = () => this.setState({mount: false})
+  }
+
   render() {
     return (
       <div>
-        <Counter />
+        <button onClick={this.mountCounter}>Mount Counter</button>
+        <button onClick={this.unmountCounter}>Unmount Counter</button>
+        {this.state.mount ? <Counter /> : null} 
       </div>
     )
   }
--- src/components/counter.js
+++ src/components/counter.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react'
 
 export default class Counter extends Component {
   constructor (props) {
+    console.log("constructor run")
     super(props)
 
     this.state = {
       counter: 0
     }
+    
+    this.increase = () => this.setState({counter: this.state.counter+1})
+    this.decrease = () => this.setState({counter: this.state.counter-1})    
   }
+
+  componentDidMount() {
+    console.log("componentDidMount run")
+  }
+
+  
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount run")
+  }
+
   render() {
+    console.log("render run")
     return (
       <div>
-        Counter: {this.state.counter}
+        <button onClick={this.increase}>Increase</button>
+        <button onClick={this.decrease}>Decrease</button>
+        <div>
+          Counter: {this.state.counter}
+        </div>
       </div>
     )
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate run")
+    console.log("args of componentDidUpdate:", {prevProps, prevProps, snapshot})
   }
 }
